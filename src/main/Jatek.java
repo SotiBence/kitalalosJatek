91cd763de5cbd2a1c12542ee16faf2bacba497da
CsoltiP <csolti@szamalk-szalezi.hu>
2024-06-29 10:11:38 +0200

    class Jatek void start()

--- src/main/Jatek.java
+++ src/main/Jatek.java
@@ -1,12 +1,18 @@
 package main;
 
+import java.util.Random;
+
 public class Jatek {
 
     void start() {
+        System.out.println("Gondoltam egy számra, 0..9 között...");
+        int szam = new Random().nextInt(10);//0..9
+        System.out.println("A kitalálandó szám: " + szam);
+        
         Jatekos jatekos1 = new Jatekos();
         Jatekos jatekos2 = new Jatekos();
-        System.out.println("jatekos1.tipp: " + jatekos1.tipp);
-        jatekos1.tipp = 7;
-        System.out.println("tippelés után --> jatekos1.tipp: " + jatekos1.tipp);
+        Jatekos jatekos3 = new Jatekos();
+        
+        /* a játék menete, amíg vaki(k) nem talál */
     }
 }
