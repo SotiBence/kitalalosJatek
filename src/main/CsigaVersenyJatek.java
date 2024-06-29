0b9d2b695be9b1f3353165c7be852a3d4a9e28b8
CsoltiP <csolti@szamalk-szalezi.hu>
2024-06-29 13:47:40 +0200

    oroklodes es polimorfizmus

--- src/main/CsigaVersenyJatek.java
+++ src/main/CsigaVersenyJatek.java
@@ -1,5 +1,7 @@
 package main;
 
 public class CsigaVersenyJatek extends Jatek{
-    
+    public void indito(){
+        System.out.println("Csigaverseny elindult...");
+    }
 }
--- src/main/JatekIndito.java
+++ src/main/JatekIndito.java
@@ -10,7 +10,7 @@ public class JatekIndito {
         nem érhető el az az objektum, de 
         létezik a memóriában:
         */
-        new TippelosJatek().start();
+        //new TippelosJatek().start();
         
         /* példányosítás után adtunk referenciát az objektumra, 
         majd az objektum metódusát meghívjuk: */
@@ -21,5 +21,20 @@ public class JatekIndito {
         az osztály nevével hivatkozunk a static metódusára:
         */
         //Jatek.start();
+        
+        /* Meglévő osztály típusok használata: */
+//        TippelosJatek tippelos = new TippelosJatek();
+//        tippelos.start();
+//        
+//        CsigaVersenyJatek csigas = new CsigaVersenyJatek();
+//        csigas.indito();
+        
+        /* Öröklődés és polimorfizmus bemutatása: */
+        Jatek jatek = null;
+        jatek = new TippelosJatek();
+        ((TippelosJatek)jatek).start();
+        
+        jatek = new CsigaVersenyJatek();
+        ((CsigaVersenyJatek)jatek).indito();
     }
 }
