cdb901db94c3fbe3c2a52a8edbcd80fb5d527f68
CsoltiP <csolti@szamalk-szalezi.hu>
2024-06-29 11:14:04 +0200

    refact: Jatek --> new

--- src/main/Jatek.java
+++ src/main/Jatek.java
@@ -4,18 +4,26 @@ import java.util.Random;
 
 public class Jatek {
 
+    static Random rnd = new Random();
+    
     void start() {
         System.out.println("Gondoltam egy számra, 0..9 között...");
-        int szam = new Random().nextInt(10);//0..9
+        int szam = rnd.nextInt(10);//0..9
+        
+        Jatekos jatekos1 = new Jatekos();
+        Jatekos jatekos2 = new Jatekos();
+        Jatekos jatekos3 = new Jatekos();
         
         boolean vanTalalat;
         boolean talalt1, talalt2, talalt3;
         do {
             System.out.println("A kitalálandó szám: " + szam);
         
-            Jatekos jatekos1 = new Jatekos();
-            Jatekos jatekos2 = new Jatekos();
-            Jatekos jatekos3 = new Jatekos();
+            /* HIBA: minden körben új Jatekos jön létre, 
+            illetve a new művelet minden körben lefut, de ez "drága": */
+//            Jatekos jatekos1 = new Jatekos();
+//            Jatekos jatekos2 = new Jatekos();
+//            Jatekos jatekos3 = new Jatekos();
 
             /* a játék menete, amíg vaki(k) nem talál */
             /* ez NEM OOP megközelíatés: */
