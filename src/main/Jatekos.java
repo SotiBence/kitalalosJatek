07f667c1ffc814c7b45ccdea7c5ef85f9e0cc7b3
CsoltiP <csolti@szamalk-szalezi.hu>
2024-06-29 10:13:26 +0200

    class Jatekos int tippel()

--- src/main/Jatekos.java
+++ src/main/Jatekos.java
@@ -1,5 +1,13 @@
 package main;
 
+import java.util.Random;
+
 public class Jatekos {
     int tipp;
+    
+    int tippel(){
+        int tipp = new Random().nextInt(10);
+        System.out.println("tippelésem: " + tipp);
+        return tipp;
+    }
 }
