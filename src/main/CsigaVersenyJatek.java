a71478d387884f0f79a2a32303779eb0beedabec
Bence <Soti2200@gmail.com>
2024-07-06 11:23:16 +0200

    csigaverseny_1

--- src/main/CsigaVersenyJatek.java
+++ src/main/CsigaVersenyJatek.java
@@ -1,6 +1,12 @@
 package main;
 
+import java.util.Random;
+
 public class CsigaVersenyJatek extends Jatek{
+    
+    private static final Random rnd = new Random();
+    private Csiga
+    
     @Override
     public void start(){
         System.out.println("Csigaverseny elindult...");
